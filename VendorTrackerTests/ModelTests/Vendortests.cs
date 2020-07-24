3bd932c49dae2e89ed19fc3a6a5a9d60f9d6d253
Chris Yoon <chy.yoon@gmail.com>
2020-07-24 10:46:00 -0700

    pass properties test

--- VendorTrackerTests/ModelTests/Vendortests.cs
+++ VendorTrackerTests/ModelTests/Vendortests.cs
@@ -10,13 +10,30 @@ namespace VendorTracker.Tests
   {
     // public void Dispose()
     // {
-    // Order.ClearAll();
+    // Vendor.ClearAll();
     // }
     [TestMethod]
     public void GetObject_ReturnsVendorInstance_Vendor()
     {
-    Vendor newVendor = new Vendor();
+    Vendor newVendor = new Vendor("test", "test");
     Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+    [TestMethod]
+    public void GetNameInput_ReturnsNameInput_String()
+    {
+      string testName = "test1";
+      Vendor newVendor = new Vendor(testName,"");
+      string result = newVendor.Name;
+      Assert.AreEqual(testName, result);
+    }
+    [TestMethod]
+    public void GetProperyDescription_ReturnsDescriptionInput_String()
+    {
+      string testName = "test1";
+      string testDescription = "test2";
+      Vendor newVendor = new Vendor(testName, testDescription);
+      string result = newVendor.Description;
+      Assert.AreEqual(testDescription, result);
     }
   }
 }
