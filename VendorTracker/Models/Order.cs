5febdf7eca72558e9553f98c9401f5e73700cb2c
Chris Yoon <chy.yoon@gmail.com>
2020-07-24 10:24:12 -0700

    pass the tests for the other properties

--- VendorTracker/Models/Order.cs
+++ VendorTracker/Models/Order.cs
@@ -5,13 +5,19 @@ namespace VendorTracker.Models
 {
   public class Order
   {
-    public int Quantity{get; set;}
+    public int Price{get; set;}
+    public string Date {get; set;}
+    public string Title {get; set;}
+    public string Description{get; set;}
     public int Id { get; }
     private static List<Order> _instances = new List<Order>{};
 
-    public Order(int userInput)
+    public Order(int userPrice, string userDate, string userDescription, string userTitle)
     {
-      Quantity = userInput;
+      Price = userPrice;
+      Date = userDate;
+      Description = userDescription;
+      Title = userTitle;
       _instances.Add(this);
       Id = _instances.Count;
     }
--- VendorTrackerTests/ModelTests/OrderTests.cs
+++ VendorTrackerTests/ModelTests/OrderTests.cs
@@ -12,26 +12,57 @@ namespace VendorTracker.Tests
     {
     Order.ClearAll();
     }
-
     [TestMethod]
     public void OrderConstructor_CreatesInstanceofOrder_Order()
     {
-      Order newOrder = new Order(5);
+      Order newOrder = new Order(5, "","","");
       Assert.AreEqual(typeof(Order), newOrder.GetType());
     }
     [TestMethod]
-    public void GetQuantityInput_ReturnsQuantityInput_Int()
+    public void GetPriceInput_ReturnsPriceInput_Int()
     {
       int testOrder = 5;
-      Order newOrder = new Order(testOrder);
-      int result = newOrder.Quantity;
+      Order newOrder = new Order(testOrder, "","","");
+      int result = newOrder.Price;
+
       Assert.AreEqual(testOrder, result);
     }
+    [TestMethod]
+    public void GetProperyDate_ReturnsDateInput_String()
+    {
+      string testDate = "test1";
+      string testDescription ="test2";
+      string testTitle ="test3";
+      Order newOrder = new Order(4, testDate,testDescription, testTitle);
+      string result = newOrder.Date;
+      Assert.AreEqual(testDate, result);
+    }
+    [TestMethod]
+    public void GetProperyDescription_ReturnsDescriptionInputs_String()
+    {
+      string testDate = "test1";
+      string testDescription ="test2";
+      string testTitle ="test3";
+      Order newOrder = new Order(4, testDate,testDescription, testTitle);
+      string result = newOrder.Description;
+      Assert.AreEqual(testDescription, result);
+    }
+    [TestMethod]
+    public void GetTitleInputs_ReturnsTitleInputs_String()
+    {
+      string testDate = "test1";
+      string testDescription ="test2";
+      string testTitle ="test3";
+      Order newOrder = new Order(4, testDate,testDescription, testTitle);
+      string result = newOrder.Title;
+      Assert.AreEqual(testTitle, result);
+    }
+    
     [TestMethod]
     public void GetList_ReturnsEmptyList_OrderList()
     {
       int test = 0;
-      Order newOrder = new Order(test);
+      Order newOrder = new Order(test, "","","");
       List<Order> newList = new List<Order> {newOrder};
       List<Order> result = Order.GetAll();
       CollectionAssert.AreEqual(newList, result);
@@ -41,13 +72,9 @@ namespace VendorTracker.Tests
     {
       int test1 = 1;
       int test2 = 2;
-      Order newOrder1 = new Order(test1);
-      Order newOrder2 = new Order(test2);
+      Order newOrder1 = new Order(test1, "","","");
+      Order newOrder2 = new Order(test2, "","","");
       List<Order> newList = new List<Order> { newOrder1, newOrder2 };
-      foreach(Order x in newList)
-      {
-        Console.WriteLine(x.Quantity);
-      }
       List<Order> result = Order.GetAll();
       CollectionAssert.AreEqual(newList, result);
     }
