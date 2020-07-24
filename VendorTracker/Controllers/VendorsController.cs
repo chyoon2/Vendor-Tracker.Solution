83a07c85de544fc8232dcc618fd724e8023da8e0
Chris Yoon <chy.yoon@gmail.com>
2020-07-24 14:11:53 -0700

    make a new routes and ui in the index

--- VendorTracker/Controllers/VendorsController.cs
+++ VendorTracker/Controllers/VendorsController.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 
 namespace VendorTracker.Controllers
 {
-  public class VendorController : Controller
+  public class VendorsController : Controller
   {
     [HttpGet("/vendors")]
     public ActionResult Index()
@@ -18,11 +18,36 @@ namespace VendorTracker.Controllers
     {
       return View();
     }
+    
     [HttpPost("/vendors")]
-    public ActionResult Create(string title, string description)
+    public ActionResult Create(string name, string description)
     {
-      Vendor newVendor = new Vendor(title, description);
-      return RedirectToAction("Index", newVendor);
+      Vendor newVendor = new Vendor(name, description);
+      return RedirectToAction("Index");
     }
+
+    [HttpGet("/vendors/{id}")]
+    public ActionResult Show(int id)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Vendor selectedVendor = Vendor.FindId(id);
+      List<Order> vendorOrders = selectedVendor.OrdersPerVendorList;
+      model.Add("vendor", selectedVendor);
+      model.Add("orders", vendorOrders);
+      return View(model);
+    }
+    //  [HttpPost("/vendors/{vendorId}/orders")]
+    // public ActionResult Create(int vendorId, string orderDescription, string orderDate, string orderTitle)
+    // {
+    //   Dictionary<string, object> model = new Dictionary<string, object>();
+    //   Vendor foundVendor = Vendor.FindId(vendorId);
+    //   Order newOrder = new Order(vendorId, orderDescription, orderDate, orderTitle);
+    //   foundVendor.AddOrder(newOrder);
+    //   List<Order> vendorOrders = foundVendor.OrdersPerVendorList;
+    //   model.Add("orders", vendorOrders);
+    //   model.Add("vendor", foundVendor);
+    //   return View("Show", model);
+    // }
+
   }
 }
